4aed4af757de343c7cf837b0cbc2e751823fd7e4
igorromero <igor.romero@ibm.com>
2024-03-02 14:18:40 -0300

    Fixing security endpoints

--- src/main/java/com/fiap/ecommerce/security/SecurityConfigurations.java
+++ src/main/java/com/fiap/ecommerce/security/SecurityConfigurations.java
@@ -27,8 +27,8 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
